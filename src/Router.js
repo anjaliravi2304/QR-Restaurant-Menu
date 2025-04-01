3e5b2717770b2a237644844862b0a74d5fff61c1
“anjaliravi2304” <ravianjali2004@gmail.com>
2025-04-01 23:14:21 +0530

    resolved errors 5rd time

--- src/Router.js
+++ src/Router.js
@@ -23,7 +23,7 @@ const Router = () => {
       <Routes>
         <Route path="/QR-Restaurant-Menu/" element={<QRLoginPage />} />
         <Route path="/QR-Restaurant-Menu/404" element={<PageNotFound />} />
-        <Route path="/QR-Restaurant-Menu/customer-menu/:id" element={<CustomerMenu />} />
+        <Route path="/customer-menu/:id" element={<CustomerMenu />} />
 
         <Route element={<ProtectedRoute />}>
           <Route path="/QR-Restaurant-Menu/admindashboard" element={<AdminDashboard />} />
